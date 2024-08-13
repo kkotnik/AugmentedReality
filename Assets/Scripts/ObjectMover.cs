ab8019c95e4099cf594a0caf7c166f77bf716f76
marija <marija.zila.s@gmail.com>
2024-08-13 17:38:25 +0200

    Gumbi za vožnjo in njihova skripta

--- Assets/Scripts/ObjectMover.cs
+++ Assets/Scripts/ObjectMover.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class ObjectMover : MonoBehaviour{
 
@@ -9,34 +11,50 @@ public class ObjectMover : MonoBehaviour{
     public float rotationSpeed = 100.0f; // speed of rotation
     public float moveSpeed = 5.0f; // speed of continuous movement
 
+    //BUTTONS
+    public Button buttonForward;
+    public Button buttonBackward;
+    public Button buttonLeft;
+    public Button buttonRight;
+
+    //ali so gumbi aktivni ?
+     private bool isMovingForward = false;
+    private bool isMovingBackward = false;
+    private bool isRotatingLeft = false;
+    private bool isRotatingRight = false;
 
     void Start(){
+        //AddEventTrigger(GameObject obj, EventTriggerType type, System.Action<BaseEventData> action)
+        AddEventTrigger(buttonForward.gameObject, EventTriggerType.PointerDown, (data) => { isMovingForward = true; });
+        AddEventTrigger(buttonForward.gameObject, EventTriggerType.PointerUp, (data) => { isMovingForward = false; });
 
-        animator = GetComponent<Animator>();
-        
+        AddEventTrigger(buttonBackward.gameObject, EventTriggerType.PointerDown, (data) => { isMovingBackward = true; });
+        AddEventTrigger(buttonBackward.gameObject, EventTriggerType.PointerUp, (data) => { isMovingBackward = false; });
+
+        AddEventTrigger(buttonLeft.gameObject, EventTriggerType.PointerDown, (data) => { isRotatingLeft = true; });
+        AddEventTrigger(buttonLeft.gameObject, EventTriggerType.PointerUp, (data) => { isRotatingLeft = false; });
+
+        AddEventTrigger(buttonRight.gameObject, EventTriggerType.PointerDown, (data) => { isRotatingRight = true; });
+        AddEventTrigger(buttonRight.gameObject, EventTriggerType.PointerUp, (data) => { isRotatingRight = false; });
     }
 
-    
-    void Update(){
-
-        if (Input.GetKey(KeyCode.W)){
+    void Update()
+    {
+        if (isMovingForward){
             MoveForward();
         }
-        if (Input.GetKey(KeyCode.S)){
+        if (isMovingBackward){
             MoveBackward();
         }
-
-        if (Input.GetKey(KeyCode.A)){
+        if (isRotatingLeft){
             RotateLeft();
         }
-        if (Input.GetKey(KeyCode.D)){
+        if (isRotatingRight){
             RotateRight();
         }
-
     }
 
     void MoveForward(){
-        // move by moveDistance units
         transform.position += transform.forward * moveSpeed * Time.deltaTime;
     }
 
@@ -45,13 +63,31 @@ public class ObjectMover : MonoBehaviour{
     }
 
     void RotateLeft(){
-        // Rotate(Vector3 eulers, Space relativeTo = Space.Self);
-        // rotationSpeed degrees per second
         transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
     }
 
     void RotateRight(){
         transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
     }
+
+    // Helper method to add event triggers - kode nisem napisala sama, jo razumem
+    private void AddEventTrigger(GameObject obj, EventTriggerType type, System.Action<BaseEventData> action){
+        EventTrigger trigger = obj.GetComponent<EventTrigger>();
+        //doda eventtrigger ce ga objekt se nima
+        if (trigger == null){
+            trigger = obj.AddComponent<EventTrigger>();
+        }
+
+        //entry pove kdaj se zacne event
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.eventID = type;
+
+        //dodamo listenerja na entry
+        entry.callback.AddListener(new UnityEngine.Events.UnityAction<BaseEventData>(action));
+        
+        //dodamo entry na trigger
+        trigger.triggers.Add(entry);
+    }
+
 
 }
